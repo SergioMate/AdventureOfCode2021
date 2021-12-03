c0a8ee381c5cb7f6f265185af77be7e2384e5d78
SergioMate <sergiomatecastro@gmail.com>
2021-12-03 22:19:34 +0100

    Fixing lint

--- app/utils.py
+++ app/utils.py
@@ -4,6 +4,7 @@ import io
 
 # Return a list of the lines of the file
 def file2list(filename):
+    """Return a list of the lines of the file"""
     with io.open(filename, 'r', encoding='utf8') as file:
         lines = file.read().splitlines()
     return lines
--- test/d01/test.py
+++ test/d01/test.py
@@ -1,22 +1,23 @@
-"""Sonat Sweep Test"""
+"""Sonar Sweep Test"""
 
 import unittest
-import pytest
 import os
 
 from app.d01.sonarsweep import number_of_increments
 from app.utils import file2list
 
-@pytest.mark.unit
 class TestD01(unittest.TestCase):
+    """Sonar Sweep Test"""
     def setUp(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.input_path = os.path.join(dir_path, 'input.txt')
 
     def test_number_of_increments(self):
+        """Test the number of increments without any window"""
         self.assertEqual(7, number_of_increments(file2list(self.input_path)))
 
     def test_number_of_increments_3(self):
+        """Test the number of increments with a window of 3"""
         self.assertEqual(5, number_of_increments(file2list(self.input_path), 3))
 
 if __name__ == "__main__":
