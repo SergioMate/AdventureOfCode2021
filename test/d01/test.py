737b2b1006ddfc22df6b396e63232544095d4e3a
SergioMate <sergiomatecastro@gmail.com>
2021-12-03 22:46:29 +0100

    Fixing lint

--- test/d01/test.py
+++ test/d01/test.py
@@ -13,16 +13,14 @@ class TestD01(unittest.TestCase):
         dir_path = os.path.dirname(os.path.realpath(__file__))
         self.input_path = os.path.join(dir_path, 'input.txt')
 
-
     def test_number_of_increments(self):
         """Test the number of increments without any window"""
         self.assertEqual(7, number_of_increments(file2list(self.input_path)))
 
-
     def test_number_of_increments_3(self):
         """Test the number of increments with a window of 3"""
-        self.assertEqual(5, number_of_increments(file2list(self.input_path)\
-        , 3))
+        self.assertEqual(5, number_of_increments(file2list(self.input_path) \
+            , 3))
 
 
 if __name__ == "__main__":
