a7a5162eb833b4902c5055d6095a57e37b081e31
SergioMate <sergiomatecastro@gmail.com>
2021-12-17 18:37:43 +0100

    Fixed Lint

--- app/d02/dive.py
+++ app/d02/dive.py
@@ -1,7 +1,7 @@
 """Sonat Sweep"""
 
 
-class Position(object):
+class Position:
     """Position"""
     def __init__(self, _horizontal_ini=0, _depth_ini=0, _aim_ini=0):
         self._horizontal = _horizontal_ini
@@ -10,55 +10,61 @@ class Position(object):
 
     @property
     def horizontal(self):
+        """horizontal property getter"""
         return self._horizontal
 
     @horizontal.setter
     def horizontal(self, value):
+        """horizontal property setter"""
         self._horizontal = value
 
     @property
     def depth(self):
+        """depth property getter"""
         return self._depth
 
     @depth.setter
     def depth(self, value):
+        """depth property setter"""
         self._depth = value
 
     @property
     def aim(self):
+        """aim property getter"""
         return self._aim
 
     @aim.setter
     def aim(self, value):
+        """aim property setter"""
         self._aim = value
 
 
 def move(previous_position, action):
     """Performs the movement action over the actual position"""
     actual_position = previous_position
-    actionSteps = action.split()
-    match actionSteps[0]:
+    action_steps = action.split()
+    match action_steps[0]:
         case 'forward':
-            actual_position.horizontal += int(actionSteps[1])
+            actual_position.horizontal += int(action_steps[1])
         case 'down':
-            actual_position.depth += int(actionSteps[1])
+            actual_position.depth += int(action_steps[1])
         case 'up':
-            actual_position.depth -= int(actionSteps[1])
+            actual_position.depth -= int(action_steps[1])
     return actual_position
 
 
 def move_advanced(previous_position, action):
     """Performs the movement action over the actual position"""
     actual_position = previous_position
-    actionSteps = action.split()
-    match actionSteps[0]:
+    action_steps = action.split()
+    match action_steps[0]:
         case 'down':
-            actual_position.aim += int(actionSteps[1])
+            actual_position.aim += int(action_steps[1])
         case 'up':
-            actual_position.aim -= int(actionSteps[1])
+            actual_position.aim -= int(action_steps[1])
         case 'forward':
-            actual_position.horizontal += int(actionSteps[1])
-            actual_position.depth += actual_position.aim * int(actionSteps[1])
+            actual_position.horizontal += int(action_steps[1])
+            actual_position.depth += actual_position.aim * int(action_steps[1])
     return actual_position
 
 
