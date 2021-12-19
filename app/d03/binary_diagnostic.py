16e0e54718ac7d29e20d44b30ec9fa07ec1c8cef
SergioMate <sergiomatecastro@gmail.com>
2021-12-19 22:24:15 +0100

    Pylint fixing

--- app/d03/binary_diagnostic.py
+++ app/d03/binary_diagnostic.py
@@ -6,20 +6,13 @@ from app.utils import bin2int
 class PowerConsumption:
     """PowerConsumption"""
     def __init__(self, binary_codes):
-        self._gamma = self.extract_gamma(binary_codes)
+        self._gamma = extract_gamma(binary_codes)
         self._epsilon = self.extract_epsilon(binary_codes)
 
-    def extract_gamma(self, binary_codes):
-        """Return gamma rate"""
-        bin_gamma = list()
-        for indx in range(0, len(binary_codes[0])):
-            bin_gamma.append(statistics.mode(list(zip(*binary_codes))[indx]))
-        return bin2int(bin_gamma)
-    
     def extract_epsilon(self, binary_codes):
         """Return epsilon rate"""
         return ~self.gamma & bin2int(["1"] * len(binary_codes[0]))
-    
+
     @property
     def gamma(self):
         """gamma property getter"""
@@ -30,6 +23,13 @@ class PowerConsumption:
         """epsilon property getter"""
         return self._epsilon
 
+
+def extract_gamma(binary_codes):
+    """Return gamma rate"""
+    bin_gamma = []
+    for indx in range(0, len(binary_codes[0])):
+        bin_gamma.append(statistics.mode(list(zip(*binary_codes))[indx]))
+    return bin2int(bin_gamma)
 
 if __name__ == '__main__':
     from app.utils import file2list
